501392d444c30dd7f1631db03a5d8465c3c15eda
John Murray <69658747+lovendatj@users.noreply.github.com>
2022-03-17 12:40:24 -0400

    Updated dependencies in package.json and included a sample route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import sequelize from 'sequelize';
 
-import db from '../database/initiaizeDB.json';
+import db from '../database/initiaizeDB.js';
 
 
 const router = express.Router();
@@ -12,5 +12,20 @@ router.get('/', (req, res) => {
 
 // @TODO - Add a route to get all the information from the DB
 // 
-// 
-// 
+// SAMPLE ROUTES EXAMPLE
+// PS: DO NOT (!!!) do this outside of this class project.
+
+// router.get('/users/:user_id', (req, res) => {
+//     try{
+//         const user = await db.Users.findAll({    <-- `Users` is the name of the table in the DB
+//             where: {
+//                 id: req.params.user_id
+//             }
+//         })
+//         res.json(user);
+
+//     } catch (err){
+//         console.log(err)
+//         res.error('Server Error');
+//     }
+// });
